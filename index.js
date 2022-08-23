b457807b209285db7075fcb5ab66869b7b6d3277
onesmus <onesmusqamara@gmail.com>
2022-08-23 11:15:44 +0300

    completed assignment

---
+++ index.js
@@ -0,0 +1,16 @@
+function receivesAFunction(callback){
+
+    callback()
+}
+
+
+function returnsANamedFunction(){
+    return function namedFunction(){
+        console.log("learning js functions");
+    };
+}
+
+function returnsAnAnonymousFunction(){
+
+    return function(){alert('bad day')};
+}
